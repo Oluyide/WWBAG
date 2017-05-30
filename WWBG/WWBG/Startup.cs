3ce4420081db36bb244524d4d24ba5c60903a27c
gbenga@splasherstech.com <festinati>
2017-05-30 15:05:24 +0100

    update

---
+++ WWBG/WWBG/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WWBG.Startup))]
+namespace WWBG
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
